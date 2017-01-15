04c3f13cf329bc090d3341a1f264782cd131ed8f
Dominik <d.p.arnold@gmx.de>
2017-01-15 18:29:46 +0100

    new changes

---
+++ 1_text_IO.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+    {
+    int n;
+    cout << "Enter an integer number: ";
+    cin >> n;
+    cout << "You entered the following number: " << n << endl;
+    }
